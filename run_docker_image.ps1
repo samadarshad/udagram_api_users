f83d28704deb29e447046a5671190fa68a6128fa
samad <samadarshad@hotmail.co.uk>
2021-03-28 17:34:36 +0100

    Workaround for timezone difference (5hr expiration)

---
+++ run_docker_image.ps1
@@ -0,0 +1,1 @@
+docker run --env-file .env -p 8080:8080 -v $HOME/.aws:/root/.aws udagram_api
--- src/aws.ts
+++ src/aws.ts
@@ -14,7 +14,7 @@ export const s3 = new AWS.S3({
 
 // Generates an AWS signed URL for retrieving objects
 export function getGetSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
+  const signedUrlExpireSeconds = 60 * 60 * 5;
 
   return s3.getSignedUrl('getObject', {
     Bucket: config.aws_media_bucket,
@@ -25,7 +25,7 @@ export function getGetSignedUrl( key: string ): string {
 
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
+  const signedUrlExpireSeconds = 60 * 60 * 5;
 
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
