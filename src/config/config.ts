dd862a14a3c41cebfe1f09143715109ec0602b9c
samad <samadarshad@hotmail.co.uk>
2021-03-29 16:29:17 +0100

    Refactored

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,9 +4,6 @@ export const config = {
   'database': process.env.POSTGRES_DB,
   'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
